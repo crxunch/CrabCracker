16d0feeed63969f5a47a376cdd2bf709be0698e6
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2023-12-28 16:59:02 -0600

    Update main.rs

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/*
+    basic functionality:
+        1. the user supplies arguments about hashing algorithm, hashlist or hash, and wordlist
+        2. read from wordlist
+        3. hash contents of wordlist and compare hashes with contents of hashlist
+        4. Store the cracked hashes in a file
+    
+    neccesary functions:
+        1. Common hashing algorithms (MD5, SHAx)
+        2. file operations
+
+    future features:
+        1. multiple hashes in one file
+        2. multithreading
+*/
