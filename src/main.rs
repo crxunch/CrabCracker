7c55d34a8d73e5be2a4e32d1b1db6d170331dab6
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2023-12-28 23:57:38 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -39,20 +39,3 @@ fn lines_from_file(filename: &str) -> Vec<String> {
     lines.remove(lines.len() - 1);
     lines
 }
-
-/*
-    basic functionality:
-        1. the user supplies arguments about hashing algorithm, hashlist or hash, and wordlist
-        2. read from wordlist
-        3. hash contents of wordlist and compare hashes with contents of hashlist
-        4. Store the cracked hashes in a file
-    
-    neccesary functions:
-        1. Common hashing algorithms (MD5, SHAx)
-        2. file operations
-
-    future features:
-        1. multiple hashes in one file
-        2. multithreading
-        3. implement parsing for /etc/shadow
-*/
