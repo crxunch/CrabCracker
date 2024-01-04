50aa9b601df8c018cd8d1d85d41ff608e445f4aa
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2024-01-03 22:58:43 -0600

    add sha512

--- src/hash_algorithms.rs
+++ src/hash_algorithms.rs
@@ -8,3 +8,12 @@ pub fn sha256_hash(input: &str) -> String {
 
     return hex_string
 }
+
+pub fn sha512_hash(input: &str) -> String {
+    let mut hasher = Sha512::new();
+    hasher.update(input.as_bytes());
+    let result = hasher.finalize();
+    let hex_string = result.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
+
+    hex_string
+}
