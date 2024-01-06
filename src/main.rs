7797273837f5cf13b26be91ec78f1cb9175dfbbc
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2024-01-05 23:31:48 -0600

    implement multi threading

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use ntlm_hash::*;
-use encoding_rs::ISO_8859_10;
-use encoding_rs_io::DecodeReaderBytesBuilder;
-// use std::thread;
-// use std::time::Duration;
+use std::thread::{self, JoinHandle};
 
 mod args;
 mod hash_algorithms;
+mod file_handling;
 
 fn main() {
 
@@ -18,15 +13,13 @@ fn main() {
     let wordlist_file = arguments.get_one::<String>("wordlist").unwrap();
     let algorithm_type = arguments.get_one::<String>("algorithm").unwrap();
 
-    let mut cracked_passwords: Vec<String> = Vec::new();
-
     let max_pass_len: usize = match algorithm_type.as_str() {
         "0" => 15,
         _ => 18_446_744_073_709_551_615,
     };
 
     // if arguments.get_one::<String>("shadow").unwrap(); != None {
-    //     parse_sshadow();
+    //     parse_shadow();
     // }
 
     let hash_fn = match algorithm_type.as_str() {
@@ -38,53 +31,41 @@ fn main() {
     };
 
     println!("\nGenerating hashlist...");
-    let mut hashes = match generate_list(hashlist_file) {
+    let hashes = match file_handling::generate_list(hashlist_file) {
         Ok(items) => items,
         Err(_) => panic!("Error generating hashlist."),
     };
 
     println!("\nGenerating wordlist...");
-    let words = match generate_list(wordlist_file) {
+    let words = match file_handling::generate_list(wordlist_file) {
         Ok(items) => items,
         Err(_) => panic!("Error generating wordlist."),
     };
 
     println!("\nCracking...\n");
 
-    for word in &words {
-        if word.len() > max_pass_len {
-            continue
-        }
-        for j in 0..(hashes.len()) {
-            if hashes[j] == hash_fn(&word) {
-                cracked_passwords.push(String::from(format!("{}: {}", word, hashes[j])));
-                hashes.remove(j);
-                break
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+
+    for hash in hashes {
+        let words_clone = words.clone();
+        
+        let handle = thread::spawn(move || {
+            for word in &words_clone {
+                if word.len() > max_pass_len {
+                    continue
+                }
+                if *hash == hash_fn(&word) {
+                    println!("{}: {}", word, hash);
+                    break
+                }
             }
-        }
+        });
+
+        handles.push(handle);
     }
 
-    println!("Cracked Hashes: \n{:#?}", cracked_passwords);
-    println!("\nUncracked Hashes: \n{:#?}", hashes);
-    
-}
-
-fn generate_list(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut items: Vec<String> = Vec::new();
-    
-    let file = File::open(filename)?;
-    let reader = BufReader::new(
-        DecodeReaderBytesBuilder::new()
-            .encoding(Some(ISO_8859_10))
-            .build(file)
-    );
-
-    for line in reader.lines() {
-        match line {
-            Ok(item) => items.push(item),
-            Err(_) => continue,
-        };
+    for handle in handles {
+        handle.join().expect("Error joining thread");
     }
-
-    Ok(items)
+    
 }
